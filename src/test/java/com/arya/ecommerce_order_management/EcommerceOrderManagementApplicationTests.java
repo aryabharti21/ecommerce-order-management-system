0d7244f4fbcf94fbb92d2c0c4f532856cdd4fab0
Arya Bharti <aryabharti21@gmail.com>
2026-07-13 23:04:36 +0530

    Phase 2: Spring Boot project scaffold (Maven, Java 21)

---
+++ src/test/java/com/arya/ecommerce_order_management/EcommerceOrderManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arya.ecommerce_order_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceOrderManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
