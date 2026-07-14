69363c5a12e2fba23c809aec48825194748d0486
Arya Bharti <arya_bharti@epam.com>
2026-07-14 22:32:51 +0530

    added flyway for database and created Product and Category entity

---
+++ src/main/java/com/arya/ecommerce_order_management/entity/Product.java
@@ -0,0 +1,55 @@
+package com.arya.ecommerce_order_management.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "product")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name", nullable = false, length = 150)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(
+            name = "category_id",
+            nullable = false
+    )
+    private Category category;
+
+    @Column(
+            name = "price",
+            nullable = false,
+            precision = 10,
+            scale = 2
+    )
+    private BigDecimal price;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
